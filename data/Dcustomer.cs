682f6aa8d81567a0197462b58faffd9f318c809a
pachasp <elovfdhfshx475@gmail.com>
2023-10-13 21:14:57 -0500

    Agregar archivos de proyecto.

---
+++ data/Dcustomer.cs
@@ -0,0 +1,62 @@
+﻿using entity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data
+{
+    internal class Dcustomer
+    {
+        private static string connectionString = "Data Source=LAPTOP-54SNKJH2\\SQLEXPRESS;Initial Catalog=FacturaDB;Integrated Security=true";
+
+        private static List<Cliente> ListarClientes(string ciudad, string region)
+        {
+            List<Cliente> clientes = new List<Cliente>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Abrir la conexión
+                connection.Open();
+                string query = "ListarClientes";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        // Verificar si hay filas
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                // Leer los datos de cada fila
+                                clientes.Add(new Cliente
+                                {
+                                    Customer_ID = int.Parse(reader["IdCliente"].ToString()),
+                                    Name = reader["NombreContacto"].ToString(),
+                                    Address = reader["Ciudad"].ToString(),
+                                    Phone = reader["Pais"].ToString(),
+                                    Active = bool.Parse(reader["active"].ToString())
+                                });
+
+                            }
+                        }
+                    }
+                }   
+
+                // Cerrar la conexión
+                connection.Close();
+
+
+            }
+            return clientes;
+
+        }
+
+    }
+}
